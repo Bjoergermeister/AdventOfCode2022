53db318acbb43ff594c4152a4051086dd4ed3181
Bjoergermeister <b.svensson@ostfalia.de>
2022-12-04 13:28:37 +0100

    Day 4 Puzzle 2

--- Day4/Day4.dart
+++ Day4/Day4.dart
@@ -1,7 +1,7 @@
 import 'dart:collection';
 import "dart:io";
 
-int puzzle1(List<String> lines) {
+int run(List<String> lines, bool onlyCountWholeOverlapps) {
   int overlappingCount = 0;
   for (int i = 0; i < lines.length; i++) {
     List<int> elves = lines[i]
@@ -14,12 +14,12 @@ int puzzle1(List<String> lines) {
         HashSet.from([for (int i = elves[0]; i <= elves[1]; i++) i]);
     HashSet<int> secondElve =
         HashSet.from([for (int i = elves[2]; i <= elves[3]; i++) i]);
-    HashSet<int> overlappingSections =
-        firstElve.union(secondElve) as HashSet<int>;
+    HashSet<int> overlap = firstElve.intersection(secondElve) as HashSet<int>;
 
     // Check if the length of the union matches one of the ranges
-    if (overlappingSections.length == firstElve.length ||
-        overlappingSections.length == secondElve.length) {
+    bool overlapsWholly = overlap.length == firstElve.length ||
+        overlap.length == secondElve.length;
+    if (overlapsWholly || (!onlyCountWholeOverlapps && overlap.length > 0)) {
       overlappingCount++;
     }
   }
@@ -31,5 +31,6 @@ void main() {
   File file = File("input.txt");
   List<String> lines = file.readAsLinesSync();
 
-  print("Puzzle 1: ${puzzle1(lines)}");
+  print("Puzzle 1: ${run(lines, true)}");
+  print("Puzzle 2: ${run(lines, false)}");
 }
