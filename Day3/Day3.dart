b8761d0a27b3d0926cba955ce9cec0a0091b543e
Bjoergermeister <b.svensson@ostfalia.de>
2022-12-04 12:56:56 +0100

    Day 3 Puzzle 2

--- Day3/Day3.dart
+++ Day3/Day3.dart
@@ -27,9 +27,44 @@ int puzzle1(List<String> lines) {
   return prioritySum;
 }
 
+int puzzle2(List<String> lines) {
+  int prioritySum = 0;
+  HashMap<int, int> items = HashMap();
+
+  for (int i = 0; i < lines.length; i++) {
+    String line = lines[i];
+
+    // Run through every item in the rucksack and save them in the HashMap. Make sure every character is only counted once!
+    HashSet<int> usedCharacters = HashSet();
+    for (int j = 0; j < line.length; j++) {
+      int character = line.codeUnitAt(j);
+      if (usedCharacters.contains(character) == false) {
+        items[character] = 1 + (items[character] ?? 0);
+        usedCharacters.add(character);
+      }
+    }
+    usedCharacters.clear(); // Clear usedCharacters for next rucksack
+
+    // After every third rucksack, get the item which is in the HashMap exactly three times
+    if (i % 3 == 2) {
+      items.removeWhere((key, value) => value < 3);
+      int badge = items.keys.elementAt(0);
+
+      // Uppercase is ASCII 65 - 90, Lowercase 97 - 122
+      bool isLowercase = (badge >= 97 && badge <= 122);
+      prioritySum += (isLowercase) ? badge - 96 : badge - (65 - 27);
+
+      items.clear();
+    }
+  }
+
+  return prioritySum;
+}
+
 void main() {
   File file = File("input.txt");
   List<String> lines = file.readAsLinesSync();
 
   print("Puzzle 1: ${puzzle1(lines)}");
+  print("Puzzle 2: ${puzzle2(lines)}");
 }
