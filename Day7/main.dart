2c1d5d0bc1d18399a76589718c92d59d36331b1f
Bjoergermeister <b.svensson@ostfalia.de>
2022-12-10 12:52:38 +0100

    Day 7 Puzzle 2

--- Day7/main.dart
+++ Day7/main.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'dart:math';
 import 'dart:io' as io;
 import "File.dart";
 import "Folder.dart";
@@ -46,10 +47,23 @@ int puzzle1() {
   return totalSize;
 }
 
+int puzzle2() {
+  final int totalDiskSize = 70000000;
+  final int freeDiskSize = totalDiskSize - folders["/"]!.size;
+  final int missingSize = 30000000 - freeDiskSize;
+  int size = folders.values
+      .where((folder) => folder.size > missingSize)
+      .map<int>((folder) => folder.size)
+      .reduce(min);
+
+  return size;
+}
+
 void main() {
   io.File file = io.File("input.txt");
   List<String> lines = file.readAsLinesSync();
   parseInput(lines);
 
   print("Puzzle 1: ${puzzle1()}");
+  print("Puzzle 2: ${puzzle2()}");
 }
