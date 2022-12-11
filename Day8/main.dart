af8c0a815f3954a7a4a7019b8ab80f43a8009904
Bjoergermeister <b.svensson@ostfalia.de>
2022-12-11 10:58:40 +0100

    Day 8 Puzzle 2

--- Day8/main.dart
+++ Day8/main.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
+import 'dart:math';
 
 late final int gridWidth;
 late final int gridHeight;
 late final List<int> grid;
+int highestScenicScore = -1;
 
 void parseInput(List<String> lines) {
   grid = List.filled(gridWidth * gridHeight, 0);
@@ -51,12 +53,52 @@ bool isVisible(int x, int y) {
   return directions.any((element) => element);
 }
 
+void scenicScore(int x, int y) {
+  int treeHeight = grid[y * gridWidth + x];
+  List<int> scenicScore = [y, gridHeight - y - 1, x, gridWidth - x - 1];
+  // Top
+  for (int dy = y - 1; dy >= 0; dy--) {
+    if (grid[dy * gridWidth + x] >= treeHeight) {
+      scenicScore[0] = y - dy;
+      break;
+    }
+  }
+
+  // Bottom
+  for (int dy = y + 1; dy < gridHeight; dy++) {
+    if (grid[dy * gridWidth + x] >= treeHeight) {
+      scenicScore[1] = dy - y;
+      break;
+    }
+  }
+
+  // Left
+  for (int dx = x - 1; dx >= 0; dx--) {
+    if (grid[y * gridWidth + dx] >= treeHeight) {
+      scenicScore[2] = x - dx;
+      break;
+    }
+  }
+
+  // Right
+  for (int dx = x + 1; dx < gridWidth; dx++) {
+    if (grid[y * gridWidth + dx] >= treeHeight) {
+      scenicScore[3] = dx - x;
+      break;
+    }
+  }
+
+  int total = scenicScore.fold(1, ((previous, current) => previous * current));
+  highestScenicScore = max(total, highestScenicScore);
+}
+
 int puzzle1() {
   // Trees on the edge are always visible and don't need to be checked
   int visibleTrees = (gridHeight * 2) + ((gridWidth - 2) * 2);
 
   for (int x = 1; x < gridWidth - 1; x++) {
     for (int y = 1; y < gridHeight - 1; y++) {
+      scenicScore(x, y);
       if (isVisible(x, y) == false) continue;
       visibleTrees++;
     }
@@ -74,4 +116,5 @@ void main() {
   parseInput(lines);
 
   print("Puzzle 1: ${puzzle1()}");
+  print("Puzzle 2: ${highestScenicScore}");
 }
